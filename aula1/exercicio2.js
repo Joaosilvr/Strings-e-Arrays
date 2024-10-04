f83705918c006401dc859ff05140a25ddf18e3c2
joaosilvr <joaovitor.dasilvaramos@gmail.com>
2024-10-03 21:54:22 -0300

    aula 1 arrays

---
+++ aula1/exercicio2.js
@@ -0,0 +1,7 @@
+let frase = prompt('digite uma frase')
+
+console.log(frase.toUpperCase());
+
+let novafrase = frase.replaceAll('o','i')
+
+console.log(novafrase,novafrase.length);
---
+++ aula1/exercicio3.js
@@ -0,0 +1,5 @@
+let racas = ['husky', 'pinscher', 'pug', 'pitbull', 'schiwawa']
+
+let numero = prompt('inserir numero de 0 a 4:')
+
+console.log('a raça é', racas[numero]);
